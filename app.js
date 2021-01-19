a42fd38cf12b39a6dc56f5e22e49c468f8ea9e97
elina <e_petrova@fanshaweonline.ca>
2021-01-19 13:22:18 -0500

    routers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const stream = require('fs');
 
 // require is more or less the same as s JS import
 // or the same as require in php 
@@ -10,8 +11,39 @@ const port = process.env.PORT || 3000; // localhost:3000
 // 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hi');
+  res.setHeader('Content-Type', 'text/html');
+
+  let url = req.url; // localhost:3000/contact -> adress bar
+
+  switch (url) {
+    // case "/": - index page; root of the project
+
+    case "/contact":
+      stream.readFile('contact.html', null, ((err, data) => {
+        if(err){
+          res.writeHead(404);
+          res.write('404 not found');
+        } else {
+          res.write(data);
+        }
+      }))
+      break;
+      case "/portfolio":
+      stream.readFile('portfolio.html', null, ((err, data) => {
+        if(err){
+          res.writeHead(404);
+          res.write('404 not found');
+        } else {
+          res.write(data);
+        }
+      }))
+      break;
+
+      default:
+        res.end('hi'); // serve up a custom error page
+  }
+
+  // res.end('hi');
 });
 
 //spy on the port, look for requests 
